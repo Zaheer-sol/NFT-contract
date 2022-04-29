9d32bdf2255e88e52423288c26f76ebd0b222e32
Electron <74273105+Olalekan20@users.noreply.github.com>
2022-04-29 02:46:34 +0100

    Add files via upload

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,14 @@
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 
-const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
 
-const RINKEBY_PRIVATE_KEY = process.env.RINKEBY_PRIVATE_KEY;
 
 module.exports = {
   solidity: "0.8.4",
   networks: {
     rinkeby: {
       url: process.env.ALCHEMY_API_KEY_URL,
-      accounts: [process.env.PRIVATE_KEY],
+      accounts: [process.env.RINKEBY_PRIVATE_KEY],
     },
   },
-}; 
+};
